82e4d6744f9e5346fc52a4e0861f6934c56c3abc
LukeGu <luke.gu.work@gmail.com>
2024-10-14 10:05:19 +1100

    feat: verification pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import ThemeSwitch from '../DarkMode/ThemeSwitch';
 
 export default function Header() {
   return (
     <div className='py-6 w-full flex flex-row justify-end'>
-      <Image
-        className='absolute left-2 top-4'
-        src='/assets/icon.png'
-        width={80}
-        height={80}
-        alt='Brand icon'
-      />
+      <Link href='/'>
+        <Image
+          className='absolute left-2 top-4'
+          src='/assets/icon.png'
+          width={80}
+          height={80}
+          alt='Brand icon'
+        />
+      </Link>
       <ThemeSwitch />
     </div>
   );
