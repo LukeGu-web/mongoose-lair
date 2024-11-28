48c901d8338a643ace9bad43833ca99f0ad967b6
LukeGu <luke.gu.work@gmail.com>
2024-11-28 20:33:18 +1100

    chore: remove negative margin

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className='bg-zinc-200 dark:bg-zinc-700 text-stone-700 dark:text-white w-full p-5 flex flex-col items-center justify-center gap-2 -mt-16'>
+    <footer className='bg-zinc-200 dark:bg-zinc-700 text-stone-700 dark:text-white w-full p-5 flex flex-col items-center justify-center gap-2'>
       <div className='w-full gap-6 flex flex-row flex-wrap items-center justify-center'>
         <Link className='text-stone-700 dark:text-white' href='/about'>
           About
