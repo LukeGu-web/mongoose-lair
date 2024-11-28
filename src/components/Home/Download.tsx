eb2bea5d9f309a25aa40a1223f4417241e239c66
LukeGu <luke.gu.work@gmail.com>
2024-11-28 14:59:59 +1100

    feat: create qr code for app store

--- src/components/Home/Download.tsx
+++ src/components/Home/Download.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 import './styles.css';
 
 export default function Download() {
@@ -9,10 +10,18 @@ export default function Download() {
       whileHover={{ scale: 1.3 }}
       className='h-64 sm:h-96 w-full sm:w-4/5 max-w-3xl m-16 bg-blue-500 mb-56 rounded-full p-6 sm:p-12'
     >
-      <div className='flex items-center justify-center'>
+      <div className='flex flex-col items-center justify-center gap-4'>
         <p className='spotlight' data-cont='Today is the day'>
           Today is the day
         </p>
+        <Image
+          className='w-1/3'
+          src='/assets/qr-code.svg'
+          width={0}
+          height={0}
+          // sizes='50vw'
+          alt='Picture of the failed email verification'
+        />
       </div>
     </motion.div>
   );
--- src/components/QRCode/QRCode.tsx
+++ src/components/QRCode/QRCode.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 
 export default function QRCode() {
   return (
@@ -8,6 +9,15 @@ export default function QRCode() {
       whileHover={{ scale: 2.4 }}
       className='h-24 w-24 rounded-xl bg-slate-100 fixed bottom-4 left-4'
       style={{ transformOrigin: 'bottom left' }}
-    />
+    >
+      <Image
+        className='w-full p-2'
+        src='/assets/qr-code.svg'
+        width={0}
+        height={0}
+        // sizes='50vw'
+        alt='Picture of the failed email verification'
+      />
+    </motion.div>
   );
 }
