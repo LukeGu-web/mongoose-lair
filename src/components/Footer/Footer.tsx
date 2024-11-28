b4e2dae1a7818cbedb8af03725a91ec60d85ead3
LukeGu <luke.gu.work@gmail.com>
2024-11-28 20:36:12 +1100

    chore: hide about page for now

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,9 @@ export default function Footer() {
   return (
     <footer className='bg-zinc-200 dark:bg-zinc-700 text-stone-700 dark:text-white w-full p-5 flex flex-col items-center justify-center gap-2'>
       <div className='w-full gap-6 flex flex-row flex-wrap items-center justify-center'>
-        <Link className='text-stone-700 dark:text-white' href='/about'>
+        {/* <Link className='text-stone-700 dark:text-white' href='/about'>
           About
-        </Link>
+        </Link> */}
         <Link href='/terms-and-conditions'>Terms and Conditions</Link>
         <Link href='/privacy-policy'>Privacy Policy</Link>
         <Link href='/support'>Contact Us</Link>
